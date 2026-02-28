0b23b731855d7221bdbcc42948e27f0296787cb2
Tejas2513 <tejasynarad12@gmail.com>
2026-02-28 13:53:42 +0530

    feat: initialize client application with React, Vite, and Tailwind CSS
    - Added package.json for project dependencies and scripts
    - Created App.css for styling components
    - Implemented App.jsx as the main application component
    - Added index.css to import Tailwind CSS
    - Set up main.jsx to render the App component
    - Configured Vite with a basic setup in vite.config.js

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div>
+      <h1 className='bg-blue-500 font-bold text-2xl'>Welcome to EduHelp</h1>
+    </div>
+  )
+}
+
+export default App
