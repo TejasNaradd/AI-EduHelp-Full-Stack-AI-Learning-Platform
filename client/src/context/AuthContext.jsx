9a0ae60a88c2259e1c66326875c141385064ca4c
Tejas2513 <tejasynarad12@gmail.com>
2026-03-07 22:40:18 +0530

    refactor: simplify user fetching logic and remove unused location handling

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -1,5 +1,4 @@
 import { createContext, useContext, useEffect, useState } from "react";
-import { useLocation } from "react-router-dom";
 import api from "../api/axios";
 
 const AuthContext = createContext();
@@ -12,7 +11,6 @@ export function AuthProvider({ children }) {
       const res = await api.get("/user/me");
       setUser(res.data.data);
     } catch (err) {
-      // ignore 401 because it just means user is not logged in
       if (err.response?.status !== 401) {
         console.error(err);
       }
@@ -20,17 +18,10 @@ export function AuthProvider({ children }) {
     }
   };
 
-const location = useLocation()
-
-useEffect(() => {
-  const publicRoutes = ["/", "/login", "/register"];
-
-  if (!publicRoutes.includes(location.pathname)) {
+  // ✅ Only runs once when app first loads
+  useEffect(() => {
     fetchUser();
-  } else {
-    setUser(null);
-  }
-}, [location.pathname]);
+  }, []);
 
   return (
     <AuthContext.Provider value={{ user, setUser, fetchUser }}>
